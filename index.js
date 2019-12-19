bbf38cce127e6ab002f2868bf8ca0fb28588e130
Your Name <you@example.com>
2019-12-19 13:44:37 +0530

    d

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ const conn = mysql.createConnection({
 app.get('/allProducts', function (req, res, next) {
     conn.connect();
     conn.query('select * from sampledb.XXIBM_PRODUCT_SKU', function (err, rows, fields) {
-        if (err) throw err
-        else{
-            console.log(rows[0].rowDataPacket)
-        }
         console.log(rows[0]);
         res.send({ success: true, result: rows[0] });
     });
