d9476d24319ab3f49a875a6c30371903e235f319
Your Name <you@example.com>
2019-12-19 18:09:18 +0530

    d

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.get('/allProducts', function (req, res, next) {
     conn.query('select s.ITEM_NUMBER , s.DESCRIPTION, s.LONG_DESCRIPTION , s.CATALOGUE_CATEGORY, s.SKU_UNIT_OF_MEASURE, s.STYLE_ITEM,'+
     's.SKU_ATTRIBUTE1,s.SKU_ATTRIBUTE2,s.SKU_ATTRIBUTE3,s.SKU_ATTRIBUTE4,s.SKU_ATTRIBUTE5,s.SKU_ATTRIBUTE6, '+
     's.SKU_ATTRIBUTE_VALUE1,s.SKU_ATTRIBUTE_VALUE2,s.SKU_ATTRIBUTE_VALUE3,s.SKU_ATTRIBUTE_VALUE4,s.SKU_ATTRIBUTE_VALUE5,s.SKU_ATTRIBUTE_VALUE6,'+
-    'm.BRAND from sampledb.XXIBM_PRODUCT_SKU s inner join XXIBM_PRODUCT_STYLE m on s.STYLE_ITEM = m.ITEM_NUMBER;', function (err, rows, fields) {
+    'm.BRAND,d.LIST_PRICE,d.DISCOUNT,d.IN_STOCK,d.PRICE_EFFECTIVE_DATE from sampledb.XXIBM_PRODUCT_SKU s inner join XXIBM_PRODUCT_STYLE m on s.STYLE_ITEM = m.ITEM_NUMBER'+
+    ' inner join XXIBM_PRODUCT_PRICING d on d.ITEM_NUMBER = s.ITEM_NUMBER;', function (err, rows, fields) {
         if(err) console.log(err)
         console.log(rows)
         res.send(rows);
