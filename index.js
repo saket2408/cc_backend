7175ee08cef050788ba31153f3d61ed6def8091a
Your Name <you@example.com>
2019-12-19 17:57:39 +0530

    d

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.get('/allProducts', function (req, res, next) {
     conn.query('select s.ITEM_NUMBER , s.DESCRIPTION, s.LONG_DESCRIPTION , s.CATALOGUE_CATEGORY, SKU_UNIT_OF_MEASURE, STYLE_ITEM,'+
     'SKU_ATTRIBUTE1,SKU_ATTRIBUTE2,SKU_ATTRIBUTE3,SKU_ATTRIBUTE4,SKU_ATTRIBUTE5,SKU_ATTRIBUTE6, '+
     'SKU_ATTRIBUTE_VALUE1,SKU_ATTRIBUTE_VALUE2,SKU_ATTRIBUTE_VALUE3,SKU_ATTRIBUTE_VALUE4,SKU_ATTRIBUTE_VALUE5,SKU_ATTRIBUTE_VALUE6,'+
-    'BRAND from sampledb.XXIBM_PRODUCT_SKU s join sampledb.XXIBM_PRODUCT_STYLE m on s.ITEM_NUMBER = m.ITEM_NUMBER;', function (err, rows, fields) {
+    'BRAND from sampledb.XXIBM_PRODUCT_SKU s inner join sampledb.XXIBM_PRODUCT_STYLE m on s.STYLE_ITEM = m.ITEM_NUMBER '+
+    'join sampledb.XXIBM_PRODUCT_STYLE c on c.ITEM_NUMBER = m.ITEM_NUMBER;', function (err, rows, fields) {
         if(err) console.log(err)
         console.log(rows)
         res.send(rows);
